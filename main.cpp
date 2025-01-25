ce040631f536566274b13202cb8e20a8fc657cd2
Justice Watkins <juslwatk.utm.edu>
2025-01-24 23:05:13 -0600

    Changed main.cpp to a program that computes a sum

--- main.cpp
+++ main.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
 
 using namespace std;
+int sum(int number );
 
 int main() {
+    int n = 0;
 
-    cout << "Hello World!";
+  cout << "Provide a number: \n";
+  cin >> n;
 
-    return 0;
+  cout << "The sum of integers up to " << n << " is " << sum(n);
+
+
+}
+
+int sum(int number ) {
+    int a = 0;
+    for(int i = 0; i < number + 1; i++){
+        a += i;
+
+    }
+    return a;
 }
