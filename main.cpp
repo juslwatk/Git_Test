b8e75c0797766be57d2c4ad2e0d94e874230f827
Justice Watkins <juslwatk.utm.edu>
2025-01-24 23:12:48 -0600

    Added a similar product function

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 int sum(int number );
+int product(int number);
 
 int main() {
     int n = 0;
@@ -9,7 +10,8 @@ int main() {
   cout << "Provide a number: \n";
   cin >> n;
 
-  cout << "The sum of integers up to " << n << " is " << sum(n);
+  cout << "The sum of integers up to " << n << " is " << sum(n) << '\n';
+  cout << "The ptoduct of integers up to " << n << " is " << product(n);
 
 
 }
@@ -18,6 +20,15 @@ int sum(int number ) {
     int a = 0;
     for(int i = 0; i < number + 1; i++){
         a += i;
+
+    }
+    return a;
+}
+
+int product(int number){
+    int a = 1;
+    for(int i = 1; i < number + 1; i++){
+        a *= i;
 
     }
     return a;
